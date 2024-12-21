a6c97c2604fbeaee384753c5c9b9dedc3071ce77
Louq_Tech <144584573+Louq-Tech@users.noreply.github.com>
2024-12-21 11:05:11 +0300

    Performance Improvement
    Using sets for formatting instead of list and minimizing the read_and_write_file in file_io.py

--- text_file_processors/file_io.py
+++ text_file_processors/file_io.py
@@ -4,10 +4,9 @@ def append_file(message, log_save_location):
 
 # This function is for backspace feature
 def read_and_write_file(log_save_location):
-    with open(log_save_location, 'r') as file:
+    with open(log_save_location, 'r+') as file:
         content = file.read()
         if content:
-            content = content[:-1]
-
-            with open("./logs.txt", 'w') as file:
-                file.write(content)
+            file.seek(0)
+            file.write(content[:-1])
+            file.truncate()
